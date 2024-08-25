150075c2233ba18d1592be66b5ba202b7f1cf685
Ranjith M C <115624052+mcranjit@users.noreply.github.com>
2024-08-25 12:54:32 +0530

    Add files via upload

---
+++ Portfolio website Task-2/script.js
@@ -0,0 +1,24 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-70px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+const headers = document.querySelectorAll(".collapsible-header");
+
+headers.forEach(header => {
+  header.addEventListener("click", () => {
+    header.classList.toggle("collapsed");
+    const content = header.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+});
